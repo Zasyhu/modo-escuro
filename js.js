693be66406027c2f3464591b0ed52ac46a67d66e
Zasyhu <freitasmiguel555@gmail.com>
2025-04-08 09:10:44 -0300

    Update js.js

--- js.js
+++ js.js
@@ -1,4 +1,26 @@
+var buttom
+
+
 const toggleButton = document.getElementById("toggle");
 toggleButton.addEventListener("click", () => {
   document.body.classList.toggle("dark-mode");
 });
+
+
+
+const hora = new Date().getHours();
+function periodoDoDia(hora) {
+    if (hora >= 5 && hora <= 12) {
+        return "Manhã";
+    } else if (hora >= 13 && hora <= 17) {
+        return "Tarde";
+    } else if (hora >= 18 && hora <= 23 || hora === 0) {
+        return "Noite";
+    } else if (hora >= 1 && hora <= 4) {
+        return "Madrugada";
+    } else {
+        return "Hora inválida";
+    }
+}
+
+alert(`O período do dia para ${hora}h é ${periodoDoDia(hora)}.`);
